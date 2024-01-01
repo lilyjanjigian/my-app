2127134a6d2360f2312243c01371da4fe7241346
Lily Janjigian <ljanjig@mit.edu>
2024-01-01 14:11:10 -0500

    Adding website pages and modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { Router } from "@reach/router";
+import NotFound from "./pages/NotFound.js";
+import NavBar from "./modules/NavBar.js";
+import Home from "./pages/Home.js";
+import Courses from "./pages/Courses.js";
+import Resume from "./pages/Resume.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Add coursework page with links to each class you've taken
+    // Add skills
+    // Add fun page
+    <>
+      <NavBar/>
+      <Router>
+        <Courses path="/"/>
+        <Courses path="/courses"/>
+        <Resume path="/resume"/>
+        <NotFound default />
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/pages/Resume.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./Resume.css";
+
+const Resume = (props) => {
+  return (
+    <>
+    <div className="Resume-Background">
+      <div className="Resume-TitleContainer">
+        <div className="Resume-Title">
+          Resume
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Resume;
